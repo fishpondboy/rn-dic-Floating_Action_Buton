8f6680f3de1ef79ef248766069baf806a049d3a3
fishpondboy <jethro.kapantow@gmail.com>
2020-01-03 11:17:44 +0800

    Init

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import FloatingButton from './FloatingButton';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./assets/map.png')}
+        resizeMode='cover'
+        style={{ width: 500, height: 900, opacity: 0.5 }}
+      />
+      <FloatingButton style={{ bottom: 100 }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  }
+});
